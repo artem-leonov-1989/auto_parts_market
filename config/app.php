4da7a5b90c3c74103dc13a6eb463091bc9cc3520
Artem Leonov <artem_leonov@hotmail.com>
2022-07-23 23:43:09 +0300

    Добавил ide_helper

--- config/app.php
+++ config/app.php
@@ -181,6 +181,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        PHPOpenSourceSaver\JWTAuth\Providers\LaravelServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
         /*
          * Package Service Providers...
