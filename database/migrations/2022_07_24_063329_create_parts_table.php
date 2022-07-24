596287f6d4749a71e98938e12043745b943bd971
Artem Leonov <artem_leonov@hotmail.com>
2022-07-24 09:41:22 +0300

    Создал детали в БД

---
+++ database/migrations/2022_07_24_063329_create_parts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 200);
+            $table->string('description', 400);
+            $table->string('manufacturer_code', 50);
+            $table->string('manufacturer', 100);
+            $table->float('price');
+            $table->integer('stock_balance')->unsigned();
+            $table->foreignId('category_id')->constrained();
+            $table->foreignId('type_id')->constrained();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parts');
+    }
+};
