426411dd55275b09d880761285525cc0995f4bda
Artem Leonov <artem_leonov@hotmail.com>
2022-07-25 23:46:36 +0300

    Сброс фильтра.

--- resources/js/сomponents/filter.vue
+++ resources/js/сomponents/filter.vue
@@ -1,38 +1,44 @@
 <template>
-    <div class="accordion" id="accordionBrands">
-        <h2 class="accordion-header" id="headingBrands">
-            <button class="accordion-button fs-4" type="button" data-bs-toggle="collapse" data-bs-target="#collapseBrand" aria-expanded="true" aria-controls="collapseBrand">
-                Автомобільні бренди
-            </button>
+    <div class="d-flex flex-column">
+        <div class="accordion" id="accordionBrands">
+            <h2 class="accordion-header" id="headingBrands">
+                <button class="accordion-button fs-4" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseBrand" aria-expanded="true" aria-controls="collapseBrand">
+                    Автомобільні бренди
+                </button>
 
-        </h2>
-        <div id="collapseBrand" class="accordion-collapse collapse show" aria-labelledby="headingBrands">
-            <div class="accordion-body">
-                <brands-filter></brands-filter>
+            </h2>
+            <div id="collapseBrand" class="accordion-collapse collapse show" aria-labelledby="headingBrands">
+                <div class="accordion-body">
+                    <brands-filter></brands-filter>
+                </div>
+            </div>
+            <h2 class="accordion-header" id="headingTypes">
+                <button class="accordion-button fs-4" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseTypes" aria-expanded="true" aria-controls="collapseTypes">
+                    Автомобільні марки
+                </button>
+
+            </h2>
+            <div id="collapseTypes" class="accordion-collapse collapse show" aria-labelledby="headingTypes">
+                <div class="accordion-body">
+                    <models-filter></models-filter>
+                </div>
+            </div>
+            <h2 class="accordion-header" id="headingCategories">
+                <button class="accordion-button fs-4" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseCategories" aria-expanded="true" aria-controls="collapseCategories">
+                    Категорії товарів
+                </button>
+
+            </h2>
+            <div id="collapseCategories" class="accordion-collapse collapse show" aria-labelledby="headingCategories">
+                <div class="accordion-body">
+                    <categories-filter></categories-filter>
+                </div>
             </div>
         </div>
-        <h2 class="accordion-header" id="headingTypes">
-            <button class="accordion-button fs-4" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTypes" aria-expanded="true" aria-controls="collapseTypes">
-                Автомобільні марки
-            </button>
-
-        </h2>
-        <div id="collapseTypes" class="accordion-collapse collapse show" aria-labelledby="headingTypes">
-            <div class="accordion-body">
-                <models-filter></models-filter>
-            </div>
-        </div>
-        <h2 class="accordion-header" id="headingCategories">
-            <button class="accordion-button fs-4" type="button" data-bs-toggle="collapse" data-bs-target="#collapseCategories" aria-expanded="true" aria-controls="collapseCategories">
-                Категорії товарів
-            </button>
-
-        </h2>
-        <div id="collapseCategories" class="accordion-collapse collapse show" aria-labelledby="headingCategories">
-            <div class="accordion-body">
-                <categories-filter></categories-filter>
-            </div>
-        </div>
+        <button class="btn btn-danger w-75 mb-3 ms-auto me-auto fs-5" @click="resetFilter">Скинути фільтр</button>
     </div>
 </template>
 
@@ -53,7 +59,11 @@ export default {
         }
     },
     methods: {
-
+        resetFilter() {
+            this.$store.commit('SET_BRAND', undefined)
+            this.$store.commit('SET_CATEGORY', undefined)
+            this.$store.commit('SET_TYPE', undefined)
+        }
     },
     computed: {
 
